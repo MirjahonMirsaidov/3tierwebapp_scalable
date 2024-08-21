5bb1a609168bf877b8f12e88cdb854bbc33b889c
MirjahonMirsaidov <mirjahonac021@gmail.com>
2024-08-21 15:33:22 +0500

    deploy

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,7 @@
-# output "db_endpoint" {
-#   value = aws_db_instance.wr_db.endpoint
-# }
-#
-# output "cache_endpoint" {
-#   value = aws_elasticache_cluster.prod_redis.configuration_endpoint
-# }
-#
-# output "elastic_ip" {
-#   value = aws_eip.prod_eip.public_ip
-# }
+output "db_endpoint" {
+  value = aws_db_instance.wr_db.endpoint
+}
+
+output "cache_endpoint" {
+  value = "${aws_elasticache_cluster.prod_redis.cache_nodes.0.address}:${aws_elasticache_cluster.prod_redis.cache_nodes.0.port}"
+}
